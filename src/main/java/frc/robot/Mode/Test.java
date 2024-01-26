93c8fd0d01700164fd785f79db9696b1e1192239
ssteensma <ssteensma@oacsd.com>
2024-01-26 15:27:10 -0500

    Comments with goals

--- src/main/java/frc/robot/Mode/Test.java
+++ src/main/java/frc/robot/Mode/Test.java
@@ -4,11 +4,16 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 public class Test {
 
+    // Declare and define a drive motor controller here
+    
     public static void Initialize() {
         CommandScheduler.getInstance().cancelAll();
     }
 
     public static void Periodic() {
+
+        // Make the drive motor spin at 30%;
+
     }
 
 }
