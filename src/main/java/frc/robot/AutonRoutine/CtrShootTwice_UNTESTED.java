b9ecb232907011d6ad305d309def7b0e8a16649d
ssteensma <ssteensma@oacsd.com>
2024-03-08 12:44:24 -0500

    Comment clean-up

--- src/main/java/frc/robot/AutonRoutine/CtrShootTwice_UNTESTED.java
+++ src/main/java/frc/robot/AutonRoutine/CtrShootTwice_UNTESTED.java
@@ -18,7 +18,7 @@ public class CtrShootTwice_UNTESTED {
                 Stage.WaitForMinDuration( Auton.Shoot_Duration );
                 break;
 
-            case 3: // DRIVE SOUTH (IF Needed)
+            case 3: // DRIVE SOUTH (If Needed)
                 // Autopilot.TranslateS( Auton.DriveSpeed );
                 // Stage.WaitForMinDistance( 18 );
                 break;
@@ -35,38 +35,33 @@ public class CtrShootTwice_UNTESTED {
                 Stage.WaitForMinDistance( 12 );
                 break;
 
-            case 6: // DRIVE BACK TO APRIL TAG
-                Autopilot.TranslateN_UNTESTED( Auton.DriveSpeed );
-                Stage.WaitForMinDistance( 20 );
-                break;
-
-            case 7: // TURN TO HEADING ZERO
+            case 6: // TURN TO HEADING ZERO
                 Autopilot.AdjustToHeading( 0 );
                 Stage.WaitForHeading( 0, 1 );
                 break;
 
-            case 8: //  SLIDE TO BE ALIGNED WITH APRIL TAG CENTER
+            case 7: //  SLIDE TO BE ALIGNED WITH APRIL TAG CENTER
                 if ( CamIntake.TX() > 0 ) { Autopilot.TranslateE_UNTESTED( Auton.DriveSpeed ); }
                 else                      { Autopilot.TranslateW_UNTESTED( Auton.DriveSpeed ); }
                 Stage.WaitForAprilTagX( 0, 2 );
                 break;
 
-            case 9: // DRIVE TO APRIL TAG
+            case 8: // DRIVE TO APRIL TAG
                 Autopilot.DriveToAprilTag_UNTESTED();
                 Stage.WaitForAprilTagY( -13, 1 );
                 break;
 
-            case 10: // PREPARE TO SHOOT
+            case 9: // PREPARE TO SHOOT
                 Shooter.Shoot( 1.00 );
                 Stage.WaitForMinDuration( Auton.Shoot_PrepTime );
                 break;
 
-            case 11: // SHOOT SECOND NOTE
+            case 10: // SHOOT SECOND NOTE
                 Mechanism.ShootHi();
                 Stage.WaitForMinDuration( Auton.Shoot_Duration );
                 break;
 
-            case 12: // RECROSS LINE
+            case 11: // RECROSS LINE
                 Autopilot.TranslateS( Auton.DriveSpeed );
                 Stage.WaitForMinDistance( 48 );
                 break;
